4cec60d98a9771efba781c90df3a779696790377
Silvan Schmidt <silvan.genius@gmail.com>
2023-02-19 17:24:46 +0100

    Add comments

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -24,11 +24,11 @@ return {
                 -- basic actions
                 w = {
                     function()
-                        local file_name = vim.fn.bufname()
-                        if file_name == "" then
-                            vim.api.nvim_feedkeys(":w ", "n", true)
+                        local file_name = vim.fn.bufname() -- get current file name
+                        if file_name == "" then -- is it an empty buffer?
+                            vim.api.nvim_feedkeys(":w ", "n", true) -- yes, prepare write command for user to type the filename in
                         else
-                            vim.cmd([[ write ]])
+                            vim.cmd([[ write ]]) -- no, already is a file, just save
 			            end
                     end,
                     "Save the file",
